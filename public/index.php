5253066eed7809d3560f9666612da5e758770d85
Alban <utilisateur@macbookpro.home>
2026-05-21 13:42:11 +0200

    add custom env

--- public/index.php
+++ public/index.php
@@ -1,8 +1,13 @@
 <?php
 
+$custom = getenv('CUSTOM_ENV') ?: 'default';
+$custom = htmlspecialchars($custom, ENT_QUOTES, 'UTF-8');
+
 echo "
 <h1>Azure Container Apps Test for SwapnTech DevOps</h1>
+
 <p>Environment: Sandbox</p>
+<p>Deployment: {$custom}</p>
 
 <ul>
     <li><a href='/about.php'>About</a></li>
